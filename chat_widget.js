10522291e85c7804193fc3d8d83868ccb5470fca
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2023-04-22 01:46:21 -0300

    feat: add animations on click, icons, and fix messaging bubbles style

--- chat_widget.js
+++ chat_widget.js
@@ -15,12 +15,13 @@ function gatherUserData() {
   return userData;
 }
 
-function getClientID() {
+function getClientID(id) {
   const storedID = localStorage.getItem("chat_client_id");
-  if (storedID) {
+  if (storedID && storedID !== "undefined") {
     return storedID;
   } else {
-    const newID = "client-" + Date.now() + "-" + Math.random().toString(36).substr(2, 9);
+    const newID = id ? id : Math.random().toString(36).substr(2, 9);
+    console.log("New client ID:", newID);
     localStorage.setItem("chat_client_id", newID);
     return newID;
   }
@@ -35,9 +36,9 @@ function formatDateToTime(date) {
 }
 
 function makeMessageBubble(message) {
-  const { sender, content } = message;
-  var messageClass = sender === "user" ? "message-user" : "message-incoming";
-  if (!sender) messageClass = "message-error";
+  const { isFromUser, content, serverError } = message;
+  var messageClass = isFromUser ? "message-user" : "message-incoming";
+  if (serverError) messageClass = "message-error";
   const date = new Date();
   const time = formatDateToTime(date);
 
@@ -49,7 +50,7 @@ function makeMessageBubble(message) {
               </div>
             </div>
             <div class="message-time-container">
-              ${!sender ? "" : `<div class="message-time">${time}</div>`}
+              ${serverError ? "" : `<div class="message-time">${time}</div>`}
             </div>
           </div>
         </div>
@@ -57,51 +58,70 @@ function makeMessageBubble(message) {
   return element;
 }
 
+const messageHistory = [];
+
 document.addEventListener("DOMContentLoaded", function () {
-  const clientID = getClientID();
+  // Initialize the socket.io client
+  const socket = io("ws://localhost:8080");
+  feather.replace();
+
+  const clientID = getClientID(socket.id);
   const chatButton = document.getElementById("chat-button");
   const closeButton = document.getElementById("close-button");
   const chatBox = document.getElementById("chat-box");
   const sendButton = document.getElementById("send-button");
+  const sendButtonIcon = document.getElementById("send-button-icon");
   const chatInput = document.getElementById("chat-input");
   const messages = document.getElementById("chat-messages");
   let isChatOpen = false;
-
   chatButton.addEventListener("click", () => {
     chatBox.classList.toggle("hidden");
-    if (!isChatOpen) {
-      chatButton.innerHTML = `<i class="chat-icon">Close</i>`;
-    } else {
-      chatButton.innerHTML = `<i class="chat-icon">Chat</i>`;
-    }
-    isChatOpen = !isChatOpen;
-  });
+    const chatIcon = document.getElementById("chat-button-icon");
 
+    if (!isChatOpen) {
+      chatIcon.setAttribute("data-feather", "chevron-right");
+      chatIcon.classList.remove("chat-icon-closed");
+      chatIcon.classList.add("chat-icon-open");
+    } else {
+      chatIcon.setAttribute("data-feather", "message-circle");
+      chatIcon.classList.remove("chat-icon-open");
+      chatIcon.classList.add("chat-icon-closed");
+    }
+
+    isChatOpen = !isChatOpen;
+    feather.replace(); // Update the rendered icon
+  });
   closeButton.addEventListener("click", () => {
     chatBox.classList.add("hidden");
   });
-
-  // Initialize the socket.io client
-  const socket = io("ws://localhost:8080");
 
   // Handle connection errors
   socket.on("connect_error", (error) => {
     console.error("Connection error:", error);
     //add only one error message
     if (messages.innerHTML.includes("Server Error")) return;
-    messages.innerHTML += makeMessageBubble({ content: "Server Error" });
+    const newMessage = {
+      serverError: true,
+      content: "Server Error",
+    };
+    messages.innerHTML += makeMessageBubble(newMessage);
     messages.scrollTop = messages.scrollHeight;
-
+    messageHistory.push(newMessage);
     // Inform the user about the connection error
   });
 
   // Handle message events
   socket.on("message", (data) => {
     console.log("Message received:", data);
-    if (data.user !== clientID) {
-      messages.innerHTML += makeMessageBubble({ sender: "user", content: data.message });
-      messages.scrollTop = messages.scrollHeight;
-    }
+    const isFromUser = data.user === clientID;
+    console.log(isFromUser);
+    const newMessage = {
+      isFromUser,
+      content: data.message,
+    };
+    messages.innerHTML += makeMessageBubble(newMessage);
+    messages.scrollTop = messages.scrollHeight;
+    messageHistory.push(newMessage);
   });
 
   const userData = gatherUserData();
@@ -113,12 +133,8 @@ document.addEventListener("DOMContentLoaded", function () {
       socket.emit("message", {
         user: clientID,
         message,
-        userData,
+        // userData,
       });
-
-      messages.innerHTML += makeMessageBubble({ sender: "user", content: message });
-      messages.scrollTop = messages.scrollHeight;
-
       chatInput.value = "";
     }
   };
